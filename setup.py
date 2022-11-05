5b8c114294a3ac885770cdc68813bf532e662355
Nathan Kamenchu <kamenchunathan@gmail.com>
2022-11-06 02:21:06 +0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = "A simple draughts AI to play against because I'm lonely done in python, rust, haskell and nim"
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="draughts_ai", 
+        version=VERSION,
+        author="Nathan Kamenchu",
+        author_email="kamenchunathan@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+       ]
+)
